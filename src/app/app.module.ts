e30d8b26a3e0cb5a52f8df6d11e8eb0ff486576f
itamar <itamar.lev@gmail.com>
2019-07-17 12:12:56 +0300

    additional warning and success alert components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ServerComponent } from './server/server.component';
+import { SuccessAlertComponent } from './success-alert/SuccessAlert.component';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/WarningAlert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/success-alert/SuccessAlert.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './SuccessAlert.component.html',
+  styles: [ `
+    h2  {
+      color: mediumvioletred;
+    }
+  `]
+})
+export class SuccessAlertComponent {
+
+}
